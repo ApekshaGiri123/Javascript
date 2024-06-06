310b680da84342ba9c5ad64a3d25901a70bf876c
Apeksha <apekshagiri2021@gmail.com>
2024-06-06 14:37:25 +0530

    basic

---
+++ Basics/Operation.js
@@ -0,0 +1,21 @@
+// console.log(1+"2")//12
+// console.log("1"+2)//12
+// console.log("1"+2+2)//122
+// console.log(1+2+3+"2")//62
+// console.log(+true)//1
+// console.log(+"")//0
+
+// console.log(null>0)// f
+// console.log(null>=0)//true
+// console.log(null==0)//f
+// console.log(null===0)//f
+// console.log(null<0)//f
+// console.log(null<=0)//true
+
+console.log(undefined>0)
+console.log(undefined>=0)
+console.log(undefined==0)
+console.log(undefined===0)
+console.log(undefined<0)
+console.log(undefined<=0)
+// all are false
---
+++ Basics/date&time.js
@@ -0,0 +1,12 @@
+const myDate=new Date();
+console.log(myDate);
+console.log(myDate.toString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toJSON())
+console.log(myDate.toTimeString())
+
+
+// timeStamp
+let myTimestamp= Date.now()
+console.log(myTimestamp);
